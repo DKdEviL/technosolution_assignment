10bfe388ce94347c9d0b24fecd1f36968218fddc
DkdEviL <deepaknardia98@gmail.com>
2020-08-26 16:25:07 +0530

    table component is modified and upgraded for better performance

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -19,10 +19,12 @@ function table (props) {
 
     function addFun(data){
         let color = "#000";
+        let status = "Pass"
         const totalMarks = parseInt(data.marks.Maths) + parseInt(data.marks.English) + parseInt(data.marks.Science);
         
         if(!(parseInt(data.marks.Maths) >= 20 && parseInt(data.marks.English) >= 20 && parseInt(data.marks.Science))){
-            color = "#ff0000"
+            color = "#ff0000";
+            status = "Fail"
         }
         if(totalMarks > maximum){
             maximum = totalMarks;
@@ -31,7 +33,8 @@ function table (props) {
             name: capitalize(data.name),
             rollNumber: data.rollNumber,
             total: totalMarks,
-            color: color
+            color: color,
+            status: status
         };
     }
 
@@ -43,23 +46,20 @@ function table (props) {
         newDataArr.map(data => {
             if(data.total === maximum){
                 data.color = "#00ff00";
+                data.status = "Topper"
             }
         })
         return newDataArr;
     }
     return (
         <div>
-            <table>
-                <tr>
-                    <th>Students Result Board</th>
-                </tr>
-                <tr>
-                    <td>
-                    <table>
+           <table>
+                <label style={{fontWeight: "bold"}}>Students Result Board</label>
                 <tr>
                     <th>Student Name</th>
                     <th>Roll No.</th>
                     <th>Total Marks</th>
+                    <th>Status</th>
                 </tr>
                 {
                     showResult(props.data).map((data) => {
@@ -67,15 +67,12 @@ function table (props) {
                             <tr style={{color: data.color}}>
                                 <td>{data.name}</td>
                                 <td>{data.rollNumber}</td>
-                                <td>{data.total}</td>
+                                <td style={{textAlign: "right"}}>{data.total}</td>
+                                <td>{data.status}</td>
                             </tr>
                         )
                     })
-                    
                 }
-            </table>
-                    </td>
-                </tr>
             </table>
         </div>
     )
