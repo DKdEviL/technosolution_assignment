5d0686fc173391816a3e38bc861fc51a1f5ccbc6
DkdEviL <deepaknardia98@gmail.com>
2020-08-26 20:46:02 +0530

    Routing Basic implementation done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Switch, Link, Route} from 'react-router-dom';
 import data from './data/data.json';
 import Table from './components/table/table';
+import StudentInfo from './components/studentForm/studentForm'
 import './App.css';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <div>
-          <Table data={data} />
-        </div>
-        
-      </header>
+
+      <div  className="App-header">
+        <Switch>
+          <Route path="/studentInfo">
+            <Link  to="/">Result Board</Link>
+            <StudentInfo />
+          </Route>
+          <Route path="/" >
+            <Table data={data} />
+            <Link to="/studentInfo">Enter Student Details</Link>
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
