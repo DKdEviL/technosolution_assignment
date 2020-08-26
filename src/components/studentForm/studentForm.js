41378b57d8486b0a02330ad4b1034b0dd58db1e9
DkdEviL <deepaknardia98@gmail.com>
2020-08-26 20:46:41 +0530

    Started implementing Form for student Details

---
+++ src/components/studentForm/studentForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class studentForm extends Component{
+    constructor(props){
+        super(props);
+        
+    }
+    render(){
+        return(
+            <div>
+                <p>Student Form</p>
+            </div>
+        )
+    }
+
+}
+
+export default studentForm;
